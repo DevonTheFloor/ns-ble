30aa48d6562f795f94826788db97ea9050a3fdbc
DevonTheFloor <devonthefloor@free.fr>
2022-02-26 20:28:55 +0100

    First

---
+++ README.md
@@ -0,0 +1,4 @@
+# Testing NativScript bluetooth plugin   
+(localisation)[https://github.com/nativescript-community/ble]   
+   
+Here somme Buttons for testing bluetooth fonctionnalities on nativscript-vue app.
---
+++ app/components/Home.vue
@@ -0,0 +1,68 @@
+<template>
+    <Page>
+        <ActionBar>
+            <Label text="Testing Bluetooth"/>
+        </ActionBar>
+
+        <GridLayout>
+            <Label class="info">
+                <FormattedString>
+                    <Span class="fas" text="&#xf135; "/>
+                    <Span :text="message"/>
+                </FormattedString>
+            </Label>
+        </GridLayout>
+        <StackLayout>
+            <Button text="Click to BLE" @tap="isBleEnable"/>
+        </StackLayout>
+    </Page>
+</template>
+
+<script>
+import { Bluetooth } from '@nativescript-community/ble'
+
+  export default {
+    data () {
+        return {
+            ble: null
+        }
+    },
+    computed: {
+      message() {
+        return "Ble enable ok";
+      },
+    },
+    mounted() {
+    },
+    methods: {
+        /**
+         * Testing the bluetooth connection
+         */
+        isBleEnable() {
+            this.ble = new Bluetooth();
+
+            this.ble.isBluetoothEnabled()
+            .then(
+                function(enabled) {
+                    console.log("Enabled? " + enabled);
+                }
+            )
+        }
+    }
+  };
+</script>
+
+<style scoped lang="scss">
+    @import '@nativescript/theme/scss/variables/blue';
+
+    // Custom styles
+    .fas {
+        @include colorize($color: accent);
+    }
+
+    .info {
+        font-size: 20;
+        horizontal-align: center;
+        vertical-align: center;
+    }
+</style>
