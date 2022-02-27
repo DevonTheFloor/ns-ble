a28a4fec41b24171512b79f6f39ca6307c9e7c73
DevonTheFloor <devonthefloor@free.fr>
2022-02-27 01:20:22 +0100

    Second

--- app/components/Home.vue
+++ app/components/Home.vue
@@ -1,19 +1,16 @@
 <template>
     <Page>
-        <ActionBar>
+        <ActionBar tile="Bluetooth">
             <Label text="Testing Bluetooth"/>
         </ActionBar>
 
-        <GridLayout>
-            <Label class="info">
-                <FormattedString>
-                    <Span class="fas" text="&#xf135; "/>
-                    <Span :text="message"/>
-                </FormattedString>
-            </Label>
-        </GridLayout>
-        <StackLayout>
-            <Button text="Click to BLE" @tap="isBleEnable"/>
+        <StackLayout class="stac-lay">
+            <Label text="Here all the function for use bluetooth" class="h3"/>
+            <Label :text="bleMessage" />
+            <Button text="Click for BLE" @tap="isBleEnable"/>
+            <Button text="Scan Permission" @tap="scanPermission"/>
+            <Button text="Turn On" @tap="turnOn"/>
+            <Button text="Start Scanning" @tap="startScan"/>
         </StackLayout>
     </Page>
 </template>
@@ -24,13 +21,12 @@ import { Bluetooth } from '@nativescript-community/ble'
   export default {
     data () {
         return {
-            ble: null
+            ble: null,
+            bleMessage: null
         }
     },
     computed: {
-      message() {
-        return "Ble enable ok";
-      },
+
     },
     mounted() {
     },
@@ -39,14 +35,50 @@ import { Bluetooth } from '@nativescript-community/ble'
          * Testing the bluetooth connection
          */
         isBleEnable() {
-            this.ble = new Bluetooth();
-
+            this.ble = new Bluetooth()
             this.ble.isBluetoothEnabled()
             .then(
                 function(enabled) {
                     console.log("Enabled? " + enabled);
+                    this.bleMessage = `BLE enable: ${enabled}`
+                    return this.bleMessage
                 }
             )
+        },
+        scanPermission() {
+            this.ble = new Bluetooth()
+            this.ble.hasCoarseLocationPermission()
+            .then(function(granted) {
+                // if this is 'false' you probably want to call 'requestLocationPermission' now
+                console.log("Has Location Permission? " + granted);
+                });
+        },
+        /**
+         * Turn on and use bluetooth, ask for permission so
+         */
+        turnOn () {
+            this.ble = new Bluetooth()
+            this.ble.enable()
+            .then(function(enabled) {
+                // use Bluetooth features if enabled is true
+                console.log('Turn on :', enabled);
+            }
+);
+        },
+        startScan() {
+            this.ble = new Bluetooth;
+            this.ble.startScanning({
+            filters: [{serviceUUID:'180d'}],
+            seconds: 4,
+            onDiscovered: function (peripheral) {
+                console.log("Periperhal found with UUID: " + peripheral.UUID);
+                }
+            })
+            .then(function() {
+                console.log("scanning complete");
+                }, function (err) {
+                    console.log("error while scanning: " + err);
+                    });
         }
     }
   };
@@ -64,5 +96,8 @@ import { Bluetooth } from '@nativescript-community/ble'
         font-size: 20;
         horizontal-align: center;
         vertical-align: center;
+    }
+    .stac-lay {
+         vertical-align: center;
     }
 </style>
